a7e098523dd5f35c946dcbd0797339611b229a7a
scikrikci <samet0tx@gmail.com>
2023-09-17 02:55:33 +0300

    feat: contiune app

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,93 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import {ref, watchEffect} from 'vue'
+
+const alert = ref(false)
+const alertMessage = ref('')
+
+const textControlXpath = ref('')
+const textControlCss = ref('')
+
+const xpathValue = textControlXpath.value
+const cssValue = textControlCss.value
+alertMessage.value = 'OLUM DUR YAZIYOZ DAHA'
+
+// watchEffect(() => {
+//   // alertMessage.value = `${xpathValue} ≠ ${cssValue}`
+// });
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="Welcome to YAREN MEHTAP KIRCALI Web site" />
+  <div class="card-title">
+    <h1>Welcome to my Converter</h1>
+  </div>
+  <div class="container">
+    <div class="col">
+      <h4>Xpath Selector</h4>
+      <textarea class="form-control" @keydown.enter.prevent v-model="textControlXpath" rows="5"></textarea>
     </div>
-  </header>
 
-  <main>
-    <TheWelcome />
-  </main>
+    <div class="col">
+      <h4>Css Selector</h4>
+      <textarea class="form-control" @keydown.enter.prevent rows="4" v-model="textControlCss"></textarea>
+    </div>
+  </div>
+
+  <div class="alert">{{ alertMessage }}</div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+.card-title h1 {
+  font-size: 3rem;
+  font-family: "PT Serif Caption";
+  //font-weight: bold;
+}
+textarea {
+  resize: none;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.alert {
+  color: crimson;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.container {
+  width: 100%;
+  padding: 0 20rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 2rem;
+  flex-wrap: wrap;
 }
+
+.container .col {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 5px;
+}
+.container .col h4{
+  font-size: 1.2rem;
+  font-weight: bold;
+  color: #f472b6;
+}
+
+.container .col .form-control {
+  height: 7rem;
+  min-width: 500px;
+  width: 800px;
+  box-sizing: border-box;
+  padding: 10px 5px;
+  border-radius: 10px;
+  font-size: 1.1rem;
+}
+
+.container .col .form-control::placeholder {
+  font-size: 1.2rem;
+  color: #5ad52c;
+}
+
+.container .col .form-control::-webkit-scrollbar {
+  width: 0;
+}
+
 </style>
